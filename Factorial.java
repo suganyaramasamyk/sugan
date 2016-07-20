e44eb88d42f95056efc2a8c8d3345d2aa39ff159
suganyaramasamyk <sugankarthi60@gmail.com>
2016-07-20 14:42:14 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+# sugan
+public class Factorial {
+	public static void main(String[] args) {
+		
+	
+	int fact=1,n=5;
+for(int i=1;i<=n;i++)
+{
+	fact=fact*i;
+	System.out.println(fact);
+}
+if(n==0)
+{
+	System.out.println(fact);
+}
+}
+}
